93315ae46bcbe95df38515d2394204d51dae366b
coder-reetesh <reetesh4085@gmail.com>
2025-07-04 20:25:12 +0530

    Update find their sum.c

--- find their sum.c
+++ find their sum.c
@@ -1,1 +1,13 @@
+/******************************************************************************
+Write a c program to enter two number and find their sum
+*******************************************************************************/
+#include <stdio.h>
 
+int main()
+{
+    int a=5,b=10,sum;
+    sum=a+b;
+    printf("sum=%d",sum);
+
+    return 0;
+}
