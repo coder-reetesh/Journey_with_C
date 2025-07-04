b9ca3160130bf4c50f945da5788dedec238677f3
coder-reetesh <reetesh4085@gmail.com>
2025-07-04 20:29:30 +0530

    Create circle and its diameter, circumference and area.c
    Write a c program to enter radius of a circle and its diameter,
    circumference and area

---
+++ circle and its diameter, circumference and area.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+Write a c program to enter radius of a circle and its diameter,
+circumference and area
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float pi=3.1415;
+    float radius,diameter,circumference,area;
+    printf("Enter the radius =");
+    scanf("%f",&radius);
+    diameter=(2*radius);
+    circumference=(2*pi*radius);
+    area=(pi*radius*radius);
+    printf("diameter=%f\tcircumference=%f\narea=%f",diameter,circumference,area);
+    return 0;
+}
