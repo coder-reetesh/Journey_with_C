d9fa7639ac15d4bbcfb8cdda5e750af9e04b1292
coder-reetesh <reetesh4085@gmail.com>
2025-07-04 20:28:56 +0530

    Create rectangle and finds its area.c
    Write a c program to enter length and breadth of rectangle and finds its area.

---
+++ rectangle and finds its area.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+Write a c program to enter length and breadth of rectangle and finds its area.
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float length , breadth, area;
+    printf("Enter the length =");
+    scanf("%f",&length);
+    printf("Enter the breadth =");
+    scanf("%f",&breadth);
+    area=(length*breadth);
+    printf("Area=%f",area);
+
+    return 0;
+}
