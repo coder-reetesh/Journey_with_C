b490f49853447a698091d114a73787b8226a2ef7
coder-reetesh <reetesh4085@gmail.com>
2025-07-04 20:27:54 +0530

    Create rectangle and finds it perimeter.c
    Write a c program to enter length and breadth of rectangle and finds it perimeter.

---
+++ rectangle and finds it perimeter.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+Write a c program to enter length and breadth of rectangle and finds it perimeter.
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float length , breadth, perimeter;
+    printf("Enter the length =");
+    scanf("%f",&length);
+    printf("Enter the breadth =");
+    scanf("%f",&breadth);
+    perimeter=2*(length+breadth);
+    printf("Perimeter=%f",perimeter);
+    return 0;
+}
