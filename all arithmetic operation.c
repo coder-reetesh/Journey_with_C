171733d9bbacdfcf1e0f717a009a9473cd4c24ba
coder-reetesh <reetesh4085@gmail.com>
2025-07-04 20:26:24 +0530

    Create all arithmetic operation.c
    Write a c program to enter two number and perform all arithmetic operations

---
+++ all arithmetic operation.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+Write a c program to enter two number and perform all arithmetic operations
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter the value of a=");
+    scanf ("%d",&a);
+    printf("Enter the value of b=");
+    scanf ("%d",&b);
+    
+    printf("(a+b)=%d\t(a-b)=%d\n(a*b)=%d\t(a/b)=%d",(a+b),(a-b),(a*b),(a/b));
+
+    return 0;
+}
