90c5826757088cd4a0d6df34be0b0b8fc4120d39
coder-reetesh <reetesh4085@gmail.com>
2025-07-05 11:35:05 +0530

    Create 9_Fahrenheit convert to Celsius.c
    9.  Write a C program to enter temperature in Fahrenheit and 
        convert to Celsius.

---
+++ 9_Fahrenheit convert to Celsius.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+9.  Write a C program to enter temperature in Fahrenheit and 
+    convert to Celsius.
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float temperature,Celsius;
+    
+    printf("Enter temperature in fahrenheit :");
+    scanf("%f",&temperature);
+    printf("temperature= %.2f degree fahrenheit.\n",temperature);
+    Celsius=(temperature-32)*5/9;
+    printf("Celsius=%.4f degree",Celsius);
+
+    return 0;
+}
