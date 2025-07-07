f31a551abc5468b72b355a87c7b23775f9385dd2
coder-reetesh <reetesh4085@gmail.com>
2025-07-07 20:40:58 +0530

    21.Get 'n'-th bit of a number.c
    21. Write a C program to get nth bit of a number.

--- 21.Get 'n'-th bit of a number.c
+++ 21.Get 'n'-th bit of a number.c
@@ -5,14 +5,16 @@
 
 int main()
 {
-    int num,n;
+    int num,n_th_pos,bit;
     printf("Enter the number =");
     scanf("%d",&num);
     
-    printf("Enter the n =");
-    scanf("%d",&n);
-    
-    (num & (1<<n))?printf(" %d th bit is set",n) :printf(" %d th bit is not set",n);
+    printf("Enter the number to get 'n'-bit position (0-31)=");
+    scanf("%d",&n_th_pos);
+    bit=((num >> n_th_pos) & 1);
+    bit?printf("num=%d , n_th_pos= %dth bit is get.. bit=%d",num,n_th_pos,bit) :
+    printf("num=%d ,n_th_pos= %dth bit is not get.. bit=%d",num,n_th_pos,bit);
+
 
     return 0;
 }
