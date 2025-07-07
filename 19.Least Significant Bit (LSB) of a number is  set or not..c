96ae5dc58e36d41212bd5ffe75dcce04852fba89
coder-reetesh <reetesh4085@gmail.com>
2025-07-07 17:48:19 +0530

    19.Least Significant Bit (LSB) of a number is  set or not..c
    19. Write a C program to check Least Significant Bit (LSB) of a number is set or not.

--- 19.Least Significant Bit (LSB) of a number is  set or not..c
+++ 19.Least Significant Bit (LSB) of a number is  set or not..c
@@ -2,3 +2,15 @@
 19. Write a C program to check Least Significant Bit (LSB) of a number is
  set or not.
 *******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter the number =");
+    scanf("%d",&num);
+    
+    (num & 1)?printf("LSB bit is set") :printf("LSB bit is not set");
+
+    return 0;
+}
